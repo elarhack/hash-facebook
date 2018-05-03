aa90b5fcef01a267144fcbb355ed7f72b23aa888
elar colque dueñas <elarsoft.es@gmail.com>
2018-05-02 21:46:46 -0500

    Update FacebookActivity.java

--- FacebookActivity.java
+++ FacebookActivity.java
@@ -1,13 +1,39 @@
-package com.cuscoup.androidfacebooklogin;
+package com.cuscoup.facebookloguin;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
 
-public class FacebookActivity extends AppCompatActivity {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FacebookLoguin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_facebook);
+        setContentView(R.layout.activity_facebook_loguin);
+        System.out.print("KeyHashes "+KeyHashes());
+    }
+    public String KeyHashes(){
+        PackageInfo info;
+        String KeyHashes = null;
+        try {
+            info = getPackageManager().getPackageInfo("com.cuscoup.facebookloguin", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                KeyHashes = new String(Base64.encode(md.digest(), 0));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return KeyHashes;
     }
 }
